cd46969cdcb5c58d60bd98fa3f55760388b7ee9e
Super_Octo_Potato_44 <53961723+Usesir112@users.noreply.github.com>
2023-01-27 11:23:45 +0700

    TopNavBar

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <img src="../assets/background.jpg" alt="background" class="background"/>
+    </div>
+</template>
---
+++ src/components/NavigationTopBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="w-full bg-base h-24 ml-7 absolute flex -z-10"></div>
+    <div class="ml-7 h-24">
+      <ul class="flex h-full items-center ml-7 top-menu">
+        <li class="logo"><img src="../assets/logo.png" alt="logo" /></li>
+        <li><a href="#">Client</a></li>
+        <li><a href="#">Your Brand</a></li>
+        <li><a href="#">Go gamers</a></li>
+        <li><a href="#">Services</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
